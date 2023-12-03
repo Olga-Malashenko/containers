6911f02c423517343ac0c4ef4702d1d8450e6fb1
Olga Malashenko <olga_birjukova@mail.ru>
2023-12-03 23:51:43 +0300

    Test for method add wroten

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,26 @@
-// TODO: write your code here
-/*import sum from './basic';
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
 
-console.log('worked');
+  add(personage) {
+    if (this.members.has(personage)) {
+      throw new Error('Этот персонаж уже в команде');
+    } else {
+      this.members.add(personage);
+    }
+  }
 
-console.log(sum([1, 2]));*/
+  addAll([...personages]) { // ?
+    this.members.add(personages);
+  }
 
-class Team {
-    constructor() {
-        this.members = new Set();
-    }
-    add(personage) {
-       if (this.members.has(personage)) {
-        console.log('Этот персонаж уже в команде');
-            throw new Error('Этот персонаж уже в команде');
-        } else {
-            this.members.add(personage);
-        }
-    }
-    
-    addAll([...personages]) {
-        this.members.add(personages);
-    }
-    toArray() {
-        return Array.from(this.members);
-    }
+  toArray() {
+    return Array.from(this.members);
+  }
 }
 
-
-
-const team = new Team();
+/* const team = new Team();
 
 team.members.clear();
 console.log(team.members.size);
@@ -38,8 +30,6 @@ console.log(team.members);
 console.log(' after many');
 
 let personage = "Ded Moroz";
-
-
 
 team.add(personage);
 console.log(team.members);
@@ -56,7 +46,4 @@ console.log(x);
 
 personage = 'Snegurochka';
 team.add(personage);
-console.log(team.members);
-
-
-
+console.log(team.members); */
