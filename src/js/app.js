755fddd9195e2531d1bc57fe6944cb10c4b0ed99
Olga Malashenko <olga_birjukova@mail.ru>
2023-12-03 23:12:09 +0300

    Tests for ErrorRepositiry implemented

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,18 @@
-//let errorList = new Map();
+// let errorList = new Map();
 
-class ErrorRepository {
-    constructor (code, description) {
-        /*this.code = code;
-        errorList.set (this, {
-            code,
-            description
-        });*/
-        this.list = new Map([
-            [1,'First error'],
-            [2, 'Second error'],
-            [3, 'Third error']
-        ]);
-        this.list.set(code, description);
-    }
+export default class ErrorRepository {
+  constructor() {
+    this.list = new Map([
+      [1, 'First error'],
+      [2, 'Second error'],
+      [3, 'Third error'],
+    ]);
+  }
 
-    translate(code) {
-        if(this.list.has(code)) {
-            return this.list.get(code);
-        } else {
-            
-            return 'Unknown error';
-        }
-        
+  translate(code) {
+    if (this.list.has(code)) {
+      return this.list.get(code);
     }
+    return 'Unknown error';
+  }
 }
-
-const error = new ErrorRepository();
-console.log(error.list);
-console.log(error.translate(3));
-console.log(error.translate(33));
-
-
-
-
