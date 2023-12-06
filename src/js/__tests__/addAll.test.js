5486ad7867fd7b8d5bb43ffe78f1aba82f1c0de7
Olga Malashenko <olga_birjukova@mail.ru>
2023-12-06 20:22:03 +0300

    Tests for addAll written

--- src/js/__tests__/addAll.test.js
+++ src/js/__tests__/addAll.test.js
@@ -4,14 +4,14 @@ test('Should add all personages', () => {
   const team = new Team();
   const personages = ['Ivan', 'Irina', 'Inna'];
   team.addAll(personages);
-  // const expected =  ; ???
-  expect(team.members).toEqual(expected);
+  const expected = ['Ivan', 'Irina', 'Inna'];
+  expect(team.toArray()).toEqual(expected);
 });
 
 test('Not should be doubling', () => {
   const team = new Team();
   const personages = ['Ivan', 'Irina', 'Ivan', 'Inna'];
   team.addAll(personages);
-  // const expected =  ; ???
-  expect(team.members).toEqual(expected);
+  const expected = ['Ivan', 'Irina', 'Inna'];
+  expect(team.toArray()).toEqual(expected);
 });
